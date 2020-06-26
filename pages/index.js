19c795a5bfeda1881a74e25415641866cb3506ed
Luc <luc.leray@gmail.com>
2020-06-26 10:46:30 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IconCache } from '../icons/with-icon'
 import ArrowLeft from '../icons/arrow-left'
+
+const SomeComponent = () => {
+  const [showIcon, setShowIcon] = useState(true)
+
+  return (
+    <>
+      {showIcon ? <ArrowLeft size={50} color="red" /> : null}
+      <button onClick={() => setShowIcon((v) => !v)}>Toggle first icon</button>
+    </>
+  )
+}
 
 const Index = () => {
   return (
     <div>
       <IconCache.Provider value={{}}>
+        <SomeComponent />
         <ArrowLeft />
         <ArrowLeft size={30} color="blue" />
-        <ArrowLeft size={50} color="red" />
       </IconCache.Provider>
     </div>
   )
